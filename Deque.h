4726d68d53d546d6142b616e7583bbfccb8dd0a9
JayJ20000 <111758412+JayJ20000@users.noreply.github.com>
2025-04-25 22:50:11 -0500

    Update Deque.h

--- Deque.h
+++ Deque.h
@@ -1,25 +1,30 @@
+#ifndef DEQUE_H
+#define DEQUE_H
+
 class Deque {
-private:
-  int** blockMap;
-  int blockSize;      
-  int currentSize;      
-  int blockCount;     
-  int firstBlock;     
-  int firstIndex;     
-  int lastBlock;      
-  int lastIndex;      
-  void expandFront();
-  void expandBack();
-public:
-  Deque();
-  ~Deque();
-  void pushFront(int value);
-  void pushBack(int value);
-  void popFront();
-  void popBack();
-  int getFront();
-  int getBack();
-  bool isEmpty();
-  int getSize();
-  int& operator[](int index);
-};
+    private:
+      int** blockMap;
+      int blockSize;      
+      int currentSize;      
+      int blockCount;     
+      int firstBlock;     
+      int firstIndex;     
+      int lastBlock;      
+      int lastIndex;      
+      void expandFront();
+      void expandBack();
+    public:
+      Deque();
+      ~Deque();
+      void pushFront(int &value);
+      void pushBack(int &value);
+      void popFront();
+      void popBack();
+      int getFront();
+      int getBack();
+      bool isEmpty();
+      int getSize();
+      int& operator[](int index);
+      void print();
+    };
+#endif
